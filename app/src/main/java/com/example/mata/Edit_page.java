e189303dabec24a73d91aa80a448eac67aa0ab6c
riiteeshh <rp140578@gmail.com>
2022-01-28 20:49:01 +0545

    minor project

--- app/src/main/java/com/example/mata/Edit_page.java
+++ app/src/main/java/com/example/mata/Edit_page.java
@@ -97,7 +97,6 @@ public class Edit_page extends AppCompatActivity {
                 }
 
                 else {
-
                     AlertDialog.Builder builder = new AlertDialog.Builder(Edit_page.this);
                     builder.setTitle("Enter Your Password");
 
